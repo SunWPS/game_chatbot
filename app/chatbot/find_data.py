e169e4221d6813069efb87459f3b875f595b421b
SunWPS <wongsakorn.pi@ku.th>
2021-10-16 01:11:00 +0700

    move dir

--- app/chatbot/find_data.py
+++ app/chatbot/find_data.py
@@ -17,7 +17,7 @@ config = {
     'ssl_ca': "ssl/server-ca.pem",
     'ssl_cert': "ssl/client-cert.pem",
     'ssl_key': "ssl/client-key.pem",
-    'database': 'chatbotdata'
+    'database': ''
 }
 
 
--- app/chatbot/recommend.py
+++ app/chatbot/recommend.py
@@ -5,14 +5,14 @@ from mysql.connector.constants import ClientFlag
 
 
 config = {
-    'user': 'root',
+    'user': '',
     'password': '',
     'host': '',
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': "ssl/server-ca.pem",
     'ssl_cert': "ssl/client-cert.pem",
     'ssl_key': "ssl/client-key.pem",
-    'database': 'chatbotdata'
+    'database': ''
 }
 
 
